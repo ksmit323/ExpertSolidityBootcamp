ebc8af857216abe8baf1a3f91e26e5fb9f89562c
Kenneth Smith <ksmit323@gmail.com>
2023-11-09 14:02:37 +0700

    Add Hardhat

--- contracts/CaptureTheFlag/Level4.sol
+++ contracts/CaptureTheFlag/Level4.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 /////////////////// ☆☆ extropyio ☆☆ /////////////////////
 //     -漫~*'¨¯¨'*·舞~ solidity ctf ~舞·*'¨¯¨'*~漫-     //
