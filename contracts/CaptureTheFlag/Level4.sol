09c67547359813ab81c0f32e1cdaa17da29c4124
Kenneth Smith <ksmit323@gmail.com>
2023-11-10 14:32:20 +0700

    Add level4

--- contracts/CaptureTheFlag/Level4.sol
+++ contracts/CaptureTheFlag/Level4.sol
@@ -24,7 +24,19 @@ contract Level4Template {
     // stdin: 2048                  stdout: 2048 or 2**11
     // stdin: 9223372036854775808   stdout: 9223372036854775808 or 2**63
     // stdin: 0xffffffff            stdout: 2147483648 or 0x80000000 or 2**31
-    function solution(uint256 number) external pure returns (uint256) {
+    function solution(uint256 num) external pure returns (uint256) {
         // TODO: Write your solution here
+
+        /// @solidity memory-safe-assembly
+        assembly {
+            for { let mask := 0x8000000000000000000000000000000000000000000000000000000000000000 } true {
+                mask := shr(1, mask)
+            } {
+                if and(num, mask) {
+                    mstore(0x00, mask)
+                    return(0x00, 32)
+                }
+            }
+        }
     }
 }
